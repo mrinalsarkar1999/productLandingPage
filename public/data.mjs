714c88e22c6cb7bad400b7b95aeb813abfae3dcc
Mrinal <62418152+mrinalsarkar1999@users.noreply.github.com>
2024-10-06 21:18:26 +0530

    Add files via upload

---
+++ public/data.mjs
@@ -0,0 +1,27 @@
+export const data = [
+  {
+    type: "newLaunch",
+    name: "PRO MAX GAMING WIRELESS<br/>HEADSET",
+    price: 275,
+    specifications:
+      "Get the best of audiophile grade fedility with the new and improved pro max headsets",
+  },
+  {
+    type: "product",
+    name: "PRO-SOFT 30 MM",
+    price: 275,
+    specifications: "Precision Drivers",
+  },
+  {
+    type: "product",
+    name: "PRO-SERIES",
+    price: 275,
+    specifications: "Gaming Headset",
+  },
+  {
+    type: "product",
+    name: "PRO-TUNED",
+    price: 275,
+    specifications: "Esports Professionals",
+  },
+];
